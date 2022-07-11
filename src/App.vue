b0717cdba8c477e1899f9184dd908eda6b60758b
VicAffile <victoraffile@gmail.com>
2022-07-11 23:42:54 +0200

    SCSS installations

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
 </script>
 
 <template>
@@ -9,7 +9,11 @@ import HelloWorld from './components/HelloWorld.vue'
   <HelloWorld msg="Hello Vue 3 + Vite" />
 </template>
 
-<style>
+<style lang="scss">
+@import 'scss/fonctions';
+@import 'scss/mixins';
+@import 'scss/variables';
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
