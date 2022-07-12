202b1ef5a3835301134c1f936c982c7d96730c2b
VicAffile <victoraffile@gmail.com>
2022-07-12 16:47:09 +0200

    Ajout des premières variables de couleurs et de la fonction convertion en rem

--- src/App.vue
+++ src/App.vue
@@ -20,6 +20,6 @@ import HelloWorld from './components/HelloWorld.vue';
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: rem(60);
 }
 </style>
