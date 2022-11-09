ea1f6a209bd8832d5ee3661d812d68f7dff05d91
Lightuky <47103897+Lightuky@users.noreply.github.com>
2022-11-09 16:28:33 +0100

    create base articles list/details components

---
+++ src/views/ArticleDetails.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  name: "ArticleDetails"
+}
+</script>
+
+<template>
+
+</template>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/ArticlesListe.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  name: "ArticlesListe"
+}
+</script>
+
+<template>
+  <h1>Liste articles</h1>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
