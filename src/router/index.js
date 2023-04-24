4e6a6fa7d36bf60b0bbb937d1b431e1227f3e871
Lightuky <47103897+Lightuky@users.noreply.github.com>
2023-04-24 20:03:23 +0200

    Update routes

--- src/router/index.js
+++ src/router/index.js
@@ -9,20 +9,28 @@ import Secretariat from "../views/Secretariat.vue";
 import Contact from "../views/Contact.vue";
 import MentionsLegales from "../views/MentionsLegales.vue";
 import PolitiqueConfidentialite from "../views/PolitiqueConfidentialite.vue";
+import LeClub from "../views/LeClub.vue";
 
 const routes = [
-    // Pages principales
+    // Home
     {path: '/', name: 'Accueil', component: Accueil},
-    {path: '/nos-actualités', name: 'ArticlesListe', component: ArticlesListe},
-    {path: '/nos-actualités/article/:articleId', name: 'ArticleDetails', component: ArticleDetails},
+    // Pages Club
+    {path: '/le-club', name: 'LeClub', component: LeClub},
+    {path: '/le-club/nos-actualités', name: 'ArticlesListe', component: ArticlesListe},
+    {path: '/le-club/nos-actualités/article/:articleId', name: 'ArticleDetails', component: ArticleDetails},
+    {path: '/le-club/dirigeants', name: 'Dirigeants', component: Dirigeants},
+    {path: '/le-club/nos-partenaires', name: 'Partenaires', component: Partenaires},
+    {path: '/le-club/nos-photos', name: 'Photos', component: Photos},
+    {path: '/le-club/le-secretariat', name: 'Secretariat', component: Secretariat},
     // Pages secondaires
-    {path: '/dirigeants', name: 'Dirigeants', component: Dirigeants},
-    {path: '/nos-partenaires', name: 'Partenaires', component: Partenaires},
-    {path: '/nos-photos', name: 'Photos', component: Photos},
-    {path: '/le-secretariat', name: 'Secretariat', component: Secretariat},
     {path: '/nous-contacter', name: 'Contact', component: Contact},
     {path: '/mentions-legales', name: 'MentionsLegales', component: MentionsLegales},
     {path: '/politique-de-confidentialite', name: 'PolitiqueConfidentialite', component: PolitiqueConfidentialite},
+
+    // Page Header
+    {path: '/compétition', name: 'Competition'},
+    {path: '/devenir-licencié', name: 'DevenirLicencie'},
+    {path: '/réservation', name: 'Reservation'},
 ]
 const router = createRouter({
     history: createWebHistory(),
