1cd5382ba82751aee7135aadc5ea2f1f4e97960a
Lightuky <47103897+Lightuky@users.noreply.github.com>
2023-04-25 15:22:46 +0200

    fix naming

--- src/views/Partners.vue
+++ src/views/Partners.vue
@@ -15,12 +15,12 @@ export default {
   name: "Partenaires",
   data() {
     return {
-      partenaires: null,
+      partners: null,
     };
   },
   methods: {
-    getSecretaries: async function () {
-      this.secretaries = await axios.get('http://localhost:8081/api/personnes/poste/secretaire' )
+    getPartners: async function () {
+      this.partners = await axios.get('http://localhost:8081/api/partenaires' )
           .then(function (response) {
             console.log(response.data)
             return response.data
@@ -32,7 +32,7 @@ export default {
     }
   },
   created() {
-    this.getSecretaries()
+    this.getPartners()
   }
 }
 </script>
